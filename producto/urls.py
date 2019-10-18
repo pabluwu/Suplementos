77b21c3d31883e41512e6cd44b72142079c70586
Pabluwu <lopez.pablo2305@gmail.com>
2019-10-17 22:23:46 -0300

    contactoañadido

--- producto/urls.py
+++ producto/urls.py
@@ -3,4 +3,5 @@ from producto import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('producto/contacto', views.contacto, name='contacto'),
 ]
--- producto/views.py
+++ producto/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'producto/index.html', {})
+
+def contacto(request):
+    return render(request, 'producto/contacto.html',{})
