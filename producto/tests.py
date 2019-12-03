f068992c5f2fed38c73cbf844d12b987f5ab063d
Pabluwu <lopez.pablo2305@gmail.com>
2019-12-03 02:56:28 -0300

    reset email

--- producto/tests.py
+++ producto/tests.py
@@ -1,3 +1,49 @@
 from django.test import TestCase
+from .models import *
+from django.contrib.auth.models import User
+from django.utils.timezone import datetime
 
 # Create your tests here.
+
+class TestUsuario(TestCase):
+    def setUp(self):
+        user = User.objects.create_user(username= 'jose123',
+                                                first_name= 'jose',
+                                                last_name= 'perez',
+                                                password= '1jose2',
+                                                email= 'jose@jose.com')
+        phnum = 123   
+        age= datetime.today()
+        newExtendedUser = ExtendedUser(telefono=phnum, edad=age, user=user)
+
+    def testUsuarioCreado(self):
+        exists = User.objects.filter(username='jose123').exists()
+        self.assertEqual(exists, True)
+
+class TestProducto(TestCase):
+    def setUp(self):
+        marca = Marca.objects.create(idMarca=1999, nombre='Prueba1')
+        producto= Producto.objects.create(idProducto=512, nombre='PruebaProducto', precio=19999, descripcion='prueba unitaria', marcaId=marca.idMarca)
+        
+    def testProductoMarcaCreado(self):
+        marcaExiste = Marca.objects.filter(idMarca=1999).exists()
+        productoExiste = Producto.objects.filter(idProducto=512).exists()
+
+        self.assertEqual(marcaExiste, True)
+        self.assertEqual(productoExiste, True)
+
+class TestProducto(TestCase):
+    def setUp(self):
+        marca = Marca.objects.create(idMarca=1999, nombre='Prueba1')
+        producto= Producto.objects.create(idProducto=512, nombre='PruebaProducto', precio=19999, descripcion='prueba unitaria', marcaId=marca.idMarca)
+
+    def testProductoMarcaEliminar(self):
+        marca = Marca.objects.get(idMarca=1999)
+        producto = Producto.objects.get(idProducto=512)
+        producto.delete()
+        marca.delete()
+
+        marcaExiste = Marca.objects.filter(idMarca=1999).exists()
+        productoExiste = Producto.objects.filter(idProducto=512).exists()
+        self.assertEqual(marcaExiste, False)
+        self.assertEqual(productoExiste, False)
--- producto/urls.py
+++ producto/urls.py
@@ -13,11 +13,11 @@ urlpatterns = [
     path('producto/nuevo',views.nuevo, name='nuevo'),
     path('producto/login2', views.login2, name='login2'),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('producto/password_reset', PasswordResetView.as_view(template_name='producto/password_reset.html'), name='password_reset'),
+    path('producto/password_reset', PasswordResetView.as_view(template_name='producto/password_reset.html', email_template_name='producto/password_reset_email.html'), name='password_reset'),
     path('producto/password_reset/done', PasswordResetDoneView.as_view(template_name='producto/password_reset_done.html'), name='password_reset_done'),
     path('producto/password_reset/confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='producto/password_reset_confirm.html'), name='password_reset_confirm'),
     path('producto/password_reset/complete', PasswordResetCompleteView.as_view(template_name='producto/password_reset_complete.html'), name='password_reset_complete'),
-] 
+]
 
 if settings.DEBUG:
         urlpatterns += static(settings.MEDIA_URL,
